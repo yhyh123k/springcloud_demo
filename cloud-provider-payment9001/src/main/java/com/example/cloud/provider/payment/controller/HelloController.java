7011b4681bb1f307ddbcd2b8e713bbea121de2b3
sshyi <9530845+sshyi@user.noreply.gitee.com>
2022-07-14 23:56:46 +0800

    添加dockerfile-maven-plugin

--- cloud-provider-payment9001/src/main/java/com/example/cloud/provider/payment/controller/HelloController.java
+++ cloud-provider-payment9001/src/main/java/com/example/cloud/provider/payment/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.cloud.provider.payment.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @author kezhene
  */
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/a")
     public String hello(@RequestParam("id") Integer id) {
         return "hello:" + id;
     }
