e3725f103ce14ba1517ed5b73469399c0900d894
sshyi <9530845+sshyi@user.noreply.gitee.com>
2022-07-15 18:29:46 +0800

    学习Nacos使用

--- cloud-provider-payment9001/src/main/java/com/example/cloud/provider/payment/controller/HelloController.java
+++ cloud-provider-payment9001/src/main/java/com/example/cloud/provider/payment/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.cloud.provider.payment.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/hello")
+@RefreshScope
 public class HelloController {
+
+    @Value("${user.name}")
+    private String info;
 
     @GetMapping("/a")
     public String hello(@RequestParam("id") Integer id) {
-        return "hello:" + id;
+        return "hello:" + info;
     }
 }
